473b5c90157280df362ff6a0ce502adac2eb19a2
saugat4343 <np01cp4s220134@islingtoncollege.edu.np>
2024-04-06 00:23:46 +0545

    Adding Charts in the Dashboard
    Adding a Line Chart and Doughnut Chart on the dashboard page using libraries like 'chart.js' and 'react-chartjs-2'.

--- fyp/src/components/Admin/Dashboard/Dashboard.jsx
+++ fyp/src/components/Admin/Dashboard/Dashboard.jsx
@@ -1,7 +1,52 @@
-import { Grid, Box } from '@chakra-ui/react';
+import {
+  Grid,
+  Box,
+  Text,
+  Heading,
+  Stack,
+  HStack,
+  Progress,
+} from '@chakra-ui/react';
 import React from 'react';
 import cursor from '../../../assets/images/cursor.png';
 import Sidebar from '../Sidebar';
+import { RiArrowUpLine, RiArrowDownLine } from 'react-icons/ri';
+import { DoughnutChart, LineChart } from './Chart';
+
+const Databox = ({ title, qty, qtyPercentage, profit }) => (
+  <Box
+    w={['full', '20%']}
+    boxShadow={'-2px 0 10px rgba(107, 70, 193, 0.5)'}
+    p={'8'}
+    borderRadius={'lg'}
+  >
+    <Text children={title} />
+
+    <HStack spacing={'6'}>
+      <Text fontSize={'2xl'} fontWeight={'bold'} children={qty} />
+      <HStack>
+        <Text children={`${qtyPercentage}%`} />
+        {profit ? (
+          <RiArrowUpLine color="green" />
+        ) : (
+          <RiArrowDownLine color="red" />
+        )}
+      </HStack>
+    </HStack>
+    <Text opacity={0.6} children={'Since Last Month'} />
+  </Box>
+);
+
+const Bar = ({ title, value, profit }) => (
+  <Box py={'4'} px={['0', '20']}>
+    <Heading size={'sm'} children={title} mb={'2'} />
+    <HStack w={'full'} alignItems={'center'}>
+      <Text children={profit ? '0%' : `-${value}`} />
+      <Progress w="full" value={profit ? value : 0} colorScheme="purple" />
+      <Text children={`${value > 100 ? value : 100}%`} />
+    </HStack>
+  </Box>
+);
 
 const Dashboard = () => {
   return (
@@ -10,7 +55,71 @@ const Dashboard = () => {
       minH={'100vh'}
       templateColumns={['1fr', '5fr 1fr']}
     >
-      <Box></Box>
+      <Box boxSizing={'border-box'} py={'16'} px={['4', '0']}>
+        <Text
+          children={`Last change was on ${String(new Date()).split('G')[0]}`}
+          textAlign={'center'}
+          opacity={0.5}
+        />
+        <Heading
+          children="Dashboard"
+          ml={['0', '16']}
+          mb={'16'}
+          textAlign={['center', 'left']}
+        />
+        <Stack
+          direction={['column', 'row']}
+          minH="24"
+          justifyContent={'space-evenly'}
+        >
+          <Databox title="Views" qty={123} qtyPercentage={30} profit={true} />
+          <Databox title="Users" qty={34} qtyPercentage={78} profit={true} />
+          <Databox
+            title="Subscription"
+            qty={12}
+            qtyPercentage={20}
+            profit={false}
+          />
+        </Stack>
+
+        <Box
+          m={['0', '16']}
+          borderRadius={'lg'}
+          p={['0', '16']}
+          mt={['4', '16']}
+          boxShadow={'-2px 0 10px rgba(107, 70, 193, 0.5)'}
+        >
+          <Heading
+            textAlign={['center', 'left']}
+            size={'md'}
+            children="Views Graph"
+            pt={['8', '0']}
+            ml={['0', '16']}
+          />
+          <LineChart />
+        </Box>
+        <Grid templateColumns={['1fr', '2fr 1fr']}>
+          <Box p={'4'}>
+            <Heading
+              textAlign={['center', 'left']}
+              size={'md'}
+              children="Progress Bar"
+              my={'8'}
+              ml={['0', '16']}
+            />
+            <Box>
+              <Bar title="Views" value={30} profit={true} />
+              <Bar title="Users" value={78} profit={true} />
+              <Bar title="Subscription" value={20} profit={false} />
+            </Box>
+          </Box>
+          <Box p={['0', '16']} boxSizing="border-box" py={'4'}>
+            <Heading textAlign={'center'} size="md" mb={'4'} children="Users" />
+            {/* Doughnut Graph */}
+            <DoughnutChart />
+          </Box>
+        </Grid>
+      </Box>
 
       <Sidebar />
     </Grid>
