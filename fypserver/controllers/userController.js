2b583057c49b2564d2c71f2711fa1852c7899fa3
saugat4343 <np01cp4s220134@islingtoncollege.edu.np>
2024-04-17 11:13:55 +0545

    Update userController.js
    logout cookie handling

--- fypserver/controllers/userController.js
+++ fypserver/controllers/userController.js
@@ -61,6 +61,9 @@ export const logout = catchAsyncError(async (req, res, next) => {
     .status(200)
     .cookie("token", null, {
       expires: new Date(Date.now()),
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
     })
     .json({
       success: true,
